8dbfbbfe0c445a2ef325ace8a0f5e24839177181
kirby81 <etienne.ellie@gmail.com>
2018-10-09 19:00:36 +0200

    scale to large structure

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my secret key'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite://'
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevConfig,
+    'testing': TestConfig,
+    'production': ProdConfig,
+    'default': DevConfig
+}
