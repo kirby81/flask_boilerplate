d16c4596180ca450e6307df68f3d381b4a880b93
kirby81 <etienne.ellie@gmail.com>
2018-10-12 12:04:41 +0200

    Registration method

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,30 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register a new user"""
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    """Login a user"""
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -16,13 +34,14 @@ def login():
             if next is None or next.startswith('/'):
                 next = url_for('main.index')
             return redirect(next)
-        flash('Invalid username or password')
+        flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
 
 @auth.route('/logout')
 @login_required
 def logout():
+    """Logout a user"""
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
